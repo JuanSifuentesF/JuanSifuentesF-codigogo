a32786ec18c7e8b228857d54240f4990e82b8fb5
JuanSifuentes <salemgosu@gmail.com>
2021-11-04 14:56:13 -0500

    update

--- Semana 4/Dia 3/01-javascript.js
+++ Semana 4/Dia 3/01-javascript.js
@@ -1,9 +1,50 @@
-
 // console.log permite mostrar en la consola de JS, lo que se nos antoje es muy útil para saber que esta pasando internamente dentro de js
-
 console.log("Hola Mundo!");
 
 // Variables
 var nombre = "Juan Sifuentes";
+console.log(nombre); //solamente para mostrar algo
+
+//Tipos de datos
+
+//String - Texto - Cadena de texto
+var nombre2 = "Arturo";
+
+// Number - numero
+var edad = 30;
+
+console.log(edad);
+
+//Boolean - Booleano
+var esDeNoche = true; //true ó false
+console.log(esDeNoche);
+
+//para ver el tipo de dato
+console.log(typeof esDeNoche);
+
+//para cambiar de valor a una variable, me basta con llamarla, esta variable tiene que existir antes.
+
+edad = 25;
+
+console.log(edad);
+
+nombre = "Eduardo";
 
 console.log(nombre);
+
+//cambiar de tipo de dato
+
+var anio = 2021;
+
+anio = "dos mil veintiuno";
+
+console.log(anio);
+
+//JS es un lenguage que no es tipado, no pre declaramos los tipos de datos
+
+//VALORES RAROS EN VARIABLES
+
+var nota; //undefined, no esta definido 
+
+console.log(nota);
+
