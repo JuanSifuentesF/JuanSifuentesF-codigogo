e5764c43bd7d963ca0e17436c2bf3eec933f1a4e
JuanSifuentes <salemgosu@gmail.com>
2021-11-04 13:49:27 -0500

    update

---
+++ Semana 4/Dia 3/01-javascript.js
@@ -0,0 +1,9 @@
+
+// console.log permite mostrar en la consola de JS, lo que se nos antoje es muy útil para saber que esta pasando internamente dentro de js
+
+console.log("Hola Mundo!");
+
+// Variables
+var nombre = "Juan Sifuentes";
+
+console.log(nombre);
