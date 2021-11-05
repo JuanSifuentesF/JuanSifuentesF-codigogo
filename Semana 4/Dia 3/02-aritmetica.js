740e7e34f5feaeaa08783dfa8737f43f293cd85c
JuanSifuentes <salemgosu@gmail.com>
2021-11-05 11:48:33 -0500

    update

---
+++ Semana 4/Dia 3/02-aritmetica.js
@@ -0,0 +1,20 @@
+let a =100;
+let b = 4;
+let suma = a + b;
+console.log("Esto es una suma",suma);//suma
+
+// Puedo poner directamente codigo de Js dentro del console.log(para ver el resultado)
+console.log(1000+50);
+
+let resta = a - b;//resta
+console.log("Esto es una resta",resta);
+
+let multiplicacion = a * b;//multiplicacion
+console.log("Esto es una multiplicacion",multiplicacion);
+
+let division = a / b; //division
+console.log("Esto es una division",division);
+
+let residuo = 105 % 4;
+console.log("residuo o modulo",residuo);
+
---
+++ Semana 5/01-concatenacion.js
@@ -0,0 +1,20 @@
+let a = 1;
+let b = "4";
+let c = a + b;
+console.log(typeof c);
+
+let nombre = "Juan "
+
+let apellido = "Sifuentes";
+
+let nombreCompleto = nombre + apellido ;
+
+console.log(nombreCompleto);
+
+//TEMPLATE STRING / TEMPLATE LITERAL
+//Backticks alt + 96
+//${aca dentro, podemos poner codigo de JS}
+
+let texto = `Hola soy ${nombre}${apellido} y soy de Lima`;
+
+console.log(texto);
