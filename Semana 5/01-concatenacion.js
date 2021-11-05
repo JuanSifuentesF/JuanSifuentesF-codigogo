8c46eb609f087f6e7ae13b37395ab8079a3e098e
JuanSifuentes <salemgosu@gmail.com>
2021-11-05 13:03:40 -0500

    UPDATE

--- Semana 5/01-concatenacion.js
+++ Semana 5/01-concatenacion.js
@@ -15,6 +15,23 @@ console.log(nombreCompleto);
 //Backticks alt + 96
 //${aca dentro, podemos poner codigo de JS}
 
-let texto = `Hola soy ${nombre}${apellido} y soy de Lima`;
+let edad = 30;
+
+let texto = `Hola soy ${nombre}${apellido} y soy de Lima, tengo ${edad}años`;
 
 console.log(texto);
+
+//PROMPT, capturar datos, del usuario
+let cantidadMascotas = prompt(`Hola ${nombre}, cuantas mastocas tienes?`);
+//Todo lo ingresado en un prompt es string
+console.log(`Mascotas : ${cantidadMascotas}`);
+console.log(typeof cantidadMascotas);
+
+// let convertido = +cantidadMascotas;
+
+// let convertido = parseInt(cantidadMascotas); Enteros
+let convertido = parseFloat(cantidadMascotas); //Decimales
+
+console.log(typeof convertido, convertido, "PATATA");
+
+
