c4f98e3fc744ec5dec49798518d1e51ac346309f
JuanSifuentes <salemgosu@gmail.com>
2021-11-08 13:55:14 -0500

    update

--- Semana 5/Dia 1/05-condicionales.js
+++ Semana 5/Dia 1/05-condicionales.js
@@ -2,15 +2,26 @@
 
     // if(condicion)
     // condicionales trabajan con booleanos, true y false
-    if (edad >= 18) {
-        console.log('Si accedes a la primera dosis');
-    }
-    else if(edad >= 0 ){
-        console.log('Noup, tienes que esperar');
-    }
+    // if (edad >= 18) {
+    //     console.log('Si accedes a la primera dosis');
+    // }
+    // else if(edad >= 0 ){
+    //     console.log('Noup, tienes que esperar');
+    // }
     
-    else {
-        console.log('Error al ingresar datos');
-    }
+    // else {
+    //     console.log('Error al ingresar datos');
+    // }
 
+    if(edad >= 60){
+        console.log('te toca Pfizer y te toca yaaaaa');
+        // Y  &&
+    }else if(edad>= 45 && edad <= 55){
+        console.log('te toca Pfizer y te toca el finde');
+        // O = ||
+    }else if (edad == 18 || edad == 21){
+        console.log('te toca sinopharm mañana');
+    }else{
+        console.log('Anda a yopongoelhombro.com');
+    }
 
