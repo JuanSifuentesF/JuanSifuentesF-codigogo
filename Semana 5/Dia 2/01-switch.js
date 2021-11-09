f720697330b84a6bb9fca3ce3e4ad771c0efca73
JuanSifuentes <salemgosu@gmail.com>
2021-11-09 10:59:39 -0500

    update

---
+++ Semana 5/Dia 2/01-switch.js
@@ -0,0 +1,35 @@
+// let producto =  +prompt('Ingrese el código dle producto: ');
+
+// switch(producto){
+//     //case posible valor, este puede ser un num, booleano, string.
+//     case 1:
+//         console.log('Chocolate');
+//         //todo el codigo que quiera
+//         break; //corta la ejecucion
+//     case 2:
+//         console.log('Papitas');
+//         break;
+//     case 3:
+//         console.log('Galletas');
+//         break;
+//     case 4:
+//         console.log('Pollo');
+//         break;
+//     default: //equivalente a un else
+//         console.log('Código no encontrado');
+// }
+
+let horas = +prompt('Ingrese sus horas semanales: ');
+
+//si el trabajador trabaja mas de 35 horas tienes horas extra
+//si el trabajador tiene igual o mas de 30 hasta menos de 35 horas , cumplio con su jornada
+//si tiene menos de 30 se le debe mandar un aviso
+
+switch(true){
+    case horas > 35:
+        console.log('horas extra!')
+        break;
+    case horas > 30 && horas <= 35:
+        console.log('Cumplio su jornada');
+        break;
+}
