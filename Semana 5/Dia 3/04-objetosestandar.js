31bf2ac375612c8cdc25eefdcbc500bbebcbe119
JuanSifuentes <salemgosu@gmail.com>
2021-11-10 15:30:35 -0500

    update

---
+++ Semana 5/Dia 3/04-objetosestandar.js
@@ -0,0 +1,29 @@
+let gato = {
+    nombre: "Michi",
+    raza : "Persa",
+    edad: 3,
+}
+
+console.log(gato.nombre);
+//estsa es otra manera de acceder a una propiedad, dato adicional
+
+console.log(gato["gato"]);
+
+let bebidas = ["Agua","Limonada","Mojito","Pisco Sour"];
+
+console.log(bebidas.length);
+
+bebidas.push("Inka Cola");
+
+console.table(bebidas);
+
+// led edad = 20
+// new lo que hace es crear un nuevo objeto
+
+let edad = new Number("20");
+
+console.log(typeof edad);
+
+console.log(edad + 5);
+
+console.log(typeof console);
