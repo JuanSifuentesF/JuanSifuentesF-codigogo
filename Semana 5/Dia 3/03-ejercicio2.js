8e147a41eef6f95ca8799caccd0aa13fc4a92ba1
JuanSifuentes <salemgosu@gmail.com>
2021-11-10 12:25:14 -0500

    update

---
+++ Semana 5/Dia 3/03-ejercicio2.js
@@ -0,0 +1,40 @@
+let arrJuegos = [
+    {
+        nombre : "The Witcher 3",
+        precio : 60
+    },
+    {
+        nombre : "Doom",
+        precio : 50
+    },
+    {
+        nombre : "Among Us",
+        precio : 30
+    },
+    {
+        nombre : "Bloodborne",
+        precio : 90
+    },
+    {
+        nombre : "God of War (2018)",
+        precio : 120
+    },
+    {
+        nombre : "Civilization VI",
+        precio : 45
+    }
+
+    /**
+     * Preguntaremos al usuario cual es su presupuesto, para ofrecerle videojuegos que esten a su alcance.
+     * Y despues brindaremos un carrito de compras para que el usuario pueda comprar
+     */
+
+    /**
+     * 1. preguntar su presupuesto
+     * 2. comparar el presupuesto con los juegos que ofrecemos
+     * 3. segun la comparacion tener una lista de juegos a ofrecer
+     * 4. agregar al carrito, seleccionar items
+     * 5. cobrar, indicar el total
+     */
+
+]
