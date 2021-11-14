06cb716bb9890facb8a3e846a5b8871cca93d683
JuanSifuentes <salemgosu@gmail.com>
2021-11-14 18:50:00 -0500

    update

---
+++ Semana 6/Dia3/02-todo.js
@@ -0,0 +1,40 @@
+/**
+ * 1, Que datos necesito???
+ * - la tarea (string), estado la tarea(booleano)
+ * - referencia del formulario
+ * - referencia de la lista
+ * 2, Como obtengo los datos???
+ * - evento click, tomando el value del input
+ * - crear un li
+ */
+
+let miForm = document.getElementById("formToDo");
+
+let ulTareas = document.getElementById("tareas");
+
+let tareas = []; //aqui almacenare las tareas a mostrar
+
+//a, vamos a obtener el valor del input y agregarlo a tareas[]
+
+formToDo.addEventListener("submit", function (ev) {
+  //evento
+  ev.preventDefault();
+  //formulario.name_input.value
+  let nuevaTarea = formToDo.tarea.value;
+  tareas.push(nuevaTarea);
+  console.log(tareas);
+  //.reset(), reinicia todo el fomulario
+  miForm.reset();
+  dibujarTareas();
+});
+
+function dibujarTareas() {
+  //cada vez que se ejecute dibujaTareas lo primero que hago es decir el contenido HTML va a ser "" nada
+  ulTareas.innerHTML = "";
+  //se va a encargar de dibujar las tareas
+  tareas.forEach(function (item) {
+    let li = document.createElement("li");
+    li.innerText = item;
+    ulTareas.appendChild(li);
+  });
+}
