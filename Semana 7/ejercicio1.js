3fcaaf026fd5bc4965511692add2671dd68c9439
JuanSifuentes <salemgosu@gmail.com>
2021-11-14 22:52:49 -0500

    update

---
+++ Semana 7/ejercicio1.js
@@ -0,0 +1,44 @@
+//Para esta parte usar if y for
+
+// /**
+//  * Ej1,
+//  * Crear una funcion que reciba un arreglo y un nombre
+//  * Si el nombre existe dentro de la funcion retornar la posicion del nombre encontrado, caso de no encontrarlo retornar -1
+//  *
+//  * Tiempo aprox unos 5 minutos
+//  *
+//  */
+// let nombres = ["arturo","pedro","jorge","alex","david"];
+
+// let buscarNombre = prompt("Ingresar nombre");
+
+// function BuscaName (lista, nomb){;
+//     for(i = 0;i<nombres.length;i++){
+//         if(nomb == lista[i]){
+//             let mensaje =`el nombre ubicado se encuentra en: ${i}`;
+//             return mensaje;
+//         }
+//     }
+//     return -1;
+// }
+
+// console.log(BuscaName(nombres,buscarNombre));
+
+/**
+ * Como sabe la sunat si un ruc es valido??
+ *
+ *
+ */
+
+//sumar arreglos
+// let numeros = [1, 2, 3, 10, 20];
+
+//sumar arreglo mixto
+// let arregloMixto = ["4", 5, 2, "6", 10];
+
+//sumar unicamente numeros positivos
+// let numeros = [1, -2, 3, 10, -5];
+
+
+// Buscar la cantidad de palabras del siguiente arreglo
+// let palabras = ["perro", "gato", "leon", "dragon", "perro", "gato","gato"];
