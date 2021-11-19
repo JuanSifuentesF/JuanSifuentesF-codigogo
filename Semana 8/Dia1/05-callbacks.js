1e7993d5da54db2c928587a6902ad5928e691a71
JuanSifuentes <salemgosu@gmail.com>
2021-11-19 14:56:49 -0500

    update

---
+++ Semana 8/Dia1/05-callbacks.js
@@ -0,0 +1,50 @@
+//1.- Recuerden que a una variable se le puede asignar casi calquier cosa incluyendo FUNCIONES
+
+// console.log("1. estamos jugando y pedimos rappi");
+
+// setTimeout(function () {
+//   console.log("2. Esperar al Rappi hasta que llegue");
+//   comer();
+// }, 2000);
+
+// const comer = function () {
+//   console.log("3. Llega el Rappi y comemos");
+// };
+
+//console.log("3. llega el Rappi y comemos")
+
+/**
+ * tengo que buscar un registro de un alumno en una BD inmensa
+ */
+
+let alumnosDB = [
+  "Cristofer",
+  "Arturo",
+  "Masami",
+  "Juan",
+  "Fred",
+  "Nicolas",
+  "Justin",
+  "Ariana",
+  "Glenda",
+  "Graciela",
+  "Melissa",
+];
+//fCallback va a ser la referencia de una funcion que vamos a pasar como parametro
+// const buscarAlumno = function (nombre, fCallback) {
+//   //para simular que va a demorar voy a utilizar un setTimeout
+//   //esta funcion retornara true o false, para indicar si encuentra el alumno
+//   setTimeout(function () {
+//     for (let i = 0; i = alumnosDB.length; i++) {
+//       if (alumnosDB[i] == nombre) {
+//         //hemos encontrado el alumno
+//         fCallback(true);
+//         return; //cortar la ejecucion de la funcion
+//       }
+//     }
+//     //si no la encotramos
+//     fCallback(false);
+//   }, 4000);
+// };
+
+console.log("Hola")
