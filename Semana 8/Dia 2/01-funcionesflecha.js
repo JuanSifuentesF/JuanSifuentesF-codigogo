e94714d928d5cd99a846cdc1b924896c66320a94
JuanSifuentes <salemgosu@gmail.com>
2021-11-23 11:52:44 -0500

    update

---
+++ Semana 8/Dia 2/01-funcionesflecha.js
@@ -0,0 +1,22 @@
+//Funcion flecha
+const saludar = (nombre) => {
+  return `Hola soy ${nombre}`;
+};
+//si es algo sencillo directamente puede indicar en la misma linea lo que se va a retornar
+//const saludar = nombre => `Hola soy ${nombre}`;
+
+console.log(saludar("Daniel"));
+
+let Mouse = {
+  nombre: "Miguelito",
+  roer: function () {
+    console.log(`${this.nombre} roe queso`);
+    console.log(this);
+  },
+};
+
+Mouse.roer();
+
+//No usen funciones flecha como metodos de un objeto, no funciona el this
+
+//Afuerita de un objeto utilicemos funciones flecha
