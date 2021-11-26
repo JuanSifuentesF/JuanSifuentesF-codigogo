fcc05c3808eaa74537d2e9e9526c688547ebb619
JuanSifuentes <salemgosu@gmail.com>
2021-11-26 11:29:48 -0500

    update

--- Semana 8/Dia 2/02-promesas.js
+++ Semana 8/Dia 2/02-promesas.js
@@ -2,13 +2,32 @@ const hornearTorta = () => {
   //no sabemos como ira el horneado de nuestra torta
   //y que eso demora en saber
   return new Promise((resolve, reject) => {
+    //Aqui estoy forzando a que sea tru o false
+    //comunmente tendriamos una operaciones, buscar un registro
+    //obtener un elemento, obtener datos
     setTimeout(() => {
-      let estado = true;
+      let estado = false;
       if (estado == true) {
         resolve("La torta esta Ready");
       } else {
-        reject("Pos se quem :( ");
+        reject("Pos se quemo :( ");
       }
     }, 5000);
   });
 };
+
+hornearTorta()
+  .then((tortaHorneada) => {
+    console.log(tortaHorneada);
+  })
+  .catch((tortaQuemada) => {
+    console.log(tortaQuemada);
+  });
+
+// hornearTorta()
+//   .then(function(tortaHorneada) {
+//     console.log(tortaHorneada);
+//   })
+//   .catch(function(tortaQuemada) {
+//     console.log(tortaQuemada);
+//   });
