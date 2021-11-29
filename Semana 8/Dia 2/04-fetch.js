e852ad0c514c4e0ffa2a3c00b3b969b1c98adbea
JuanSifuentes <salemgosu@gmail.com>
2021-11-29 14:56:18 -0500

    update

--- Semana 8/Dia 2/04-fetch.js
+++ Semana 8/Dia 2/04-fetch.js
@@ -1,11 +1,38 @@
+//PETICION DE TIPO GET
 fetch("https://reqres.in/api/users?page=2")
   .then((respuesta) => {
-    console.log(respuesta);
+    // console.log(respuesta);
     return respuesta.json(); //parsea de texto a JS
   })
   .then((gatos) => {
-    console.log(gatos);
+    // console.log(gatos);
   });
 
+// POST - crear
 
-//   [[[21:10 hrs]]]
+let nuevoUsuario = {
+  name: "Daniel de la cruz",
+  job: "BOSS",
+};
+
+//headers - cabeceras - guia de remision
+let cabecera = {
+  method: "POST", //Metodo HTTP, POST PUT DELETE, GET
+  headers: {
+    //Que info envio, de que tipo para que el backeng sepa como procesarlo
+    "Content-type": "application/json",
+  },
+  body: JSON.stringify(nuevoUsuario),
+  //data, carga, pero hay que transformarlo a JSON
+};
+
+fetch("https://reqres.in/api/users", cabecera)
+  .then((respuesta) => {
+    return respuesta.json();
+  })
+  .then((usuarioCreado) => {
+    console.table(usuarioCreado);
+  })
+  .catch((error)=>{
+    console.log(error);
+  });
---
+++ Semana 8/Dia3/01-asyncawait.js
@@ -0,0 +1,28 @@
+// const traerData = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve("Data Obtenida!!");
+//     // reject("Error al obteer Data")
+//   });
+// };
+
+// async, me devuelve una promesa en vez de retornarme el resultado directamete
+const traerData = async () => {
+  //aca adentro yo tengo mas codigo, haga operaciones un if, etc
+  //y cuando tenga exito return si fallo throw
+  //   return "Data Obtenida con async";
+  throw "Error al obtener data con async";
+};
+
+const usarData = async () => {
+  //cuando utilicemos await, tiene que esta dentro de una funcion y esa funcion tiene que tener async
+  try {
+    //intenta eso
+    let rpta = await traerData();
+    console.log(rpta);
+  } catch (error) {
+    //y si no te funciona ejecuta esto y me dices que ha pasado
+    console.log(error);
+  }
+};
+
+usarData();
